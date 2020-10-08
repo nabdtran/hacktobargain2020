eca6b50ffad7ac7ac06efec98ff3f8bc58c37d96
Jordan Lee <jordan@jordanlee.net>
2020-10-08 22:53:23 +1100

    add hello-world in C#

---
+++ scripts/hello-world.cs
@@ -0,0 +1,17 @@
+// LANGUAGE: C#
+// AUTHOR: Jordan L
+// GITHUB: https://github.com/therealjordanlee
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
